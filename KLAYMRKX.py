f329386a885c9a7fbde91fb8be636bcb71e2a89d
meem11meem <36925364+meem11meem@users.noreply.github.com>
2018-02-28 17:49:34 +0300

    Mazin kalil

---
+++ KLAYMRKX.py
@@ -0,0 +1,22 @@
+#Introduction to the script Script scans infected sites
+#The script was designed by Mazen Khalil
+#^-^
+print("        ************Special scrip tKLAY MRKX***************")
+print('''                  KLAY MRKX ┈┈┈╱▔▔▔▔▔▔▔▔╲┈┈┈
+                            ┈┈┈▏┈┈┈┈┈┈┈┈▕┈┈┈
+                            ┈┈┈ ╭━╮┈┈╭━╮▕┈┈┈
+                            ╱▔╲╲╰━╯╱╲╰━╯╱╱▔╲
+                            ╲▂╲╲▏╮┈▔▔┈╭▕ ╲╲▂╱
+                             ╱▔╲╲┣╋╋╋╋┫ ╲╲╲
+                            ╲▂╱┈╲▂▂▂▂▂▂╱┈╲▂╱ KLAY MRKX''')
+print("***** special script for the Jordanian and Afkar organization*****")
+
+from googlesearch import *
+#When you run the script, you are asked to enter the corresponding location
+word=input('Enter the dork here : ')
+
+for KLAY in search('word',stop=500):
+    print(KLAY)
+#So you get infected sites
+#Programmer Mazen Khalil facebook
+#www.facebook.com/memw1199
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# mazin
+#Introduction to the script Script scans infected sites
+#The script was designed by Mazen Khalil
+#It is not allowed to copy or modify or put rights in your name in it and take the role of
+#the programmer in this script and speed yourself from the programmer to use your copy and 
+#paste
+How to use the script:
+When you run a script you are asked to enter the synonyms to examine it and it will 
+examine the infected sites and extracted the script has been programmed to examine 500 
+sites each time you have to enter the sites to check
+#for example
+#When you run the script, you are asked to enter the corresponding location
+Enter the dork here : php? id=
+#So you get infected sites
+#Programmer Mazen Khalil
+#facebook
+#www.facebook.com/memw1199
+^-^
